d7ccca8108471df96e90fa29f0fba974d84df6b3
DanielBenBassat <daniel.benbassat@gmail.com>
2024-09-20 12:41:15 +0300

    start communication

--- client.py
+++ client.py
@@ -1,22 +1,28 @@
 import socket
+import hashlib
+import os
 
 IP = "127.0.0.1"
 PORT = 5555
 
 
+def calculate_md5(start, end, md5_target):
+    for i in range(start, end + 1):
+        num_str = str(i).zfill(3)
+        md5_hash = hashlib.md5(num_str.encode()).hexdigest()
+        if md5_hash == md5_target:
+            return i
+    return -1
+
+
 def main():
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client_socket.connect((IP, PORT))
-    while True:
-        message = input("You: ")
+    num_cores = os.cpu_count()
 
-        # Send message to server
-        client_socket.send(message.encode())
-        # Receive echo from the server
-        response = client_socket.recv(1024).decode()
-        print(f"Server: {response}")
-
-    client_socket.close()
+    # Send num of cores to server
+    client_socket.send(str(num_cores).encode())
+    print(num_cores)
 
 
 if __name__ == "__main__":
--- server.py
+++ server.py
@@ -1,31 +1,38 @@
 import socket
 import threading
 
+
 IP = "127.0.0.1"
 PORT = 5555
+CLIENTS_SOCKETS = []
+MD5_TARGET = 'f899139df5e1059396431415e770c6dd'
+NUM_PER_CORE = 1000
+task_start = 0
+
 
 
 def handle_client(client_socket, address):
     print(f"[NEW CONNECTION] {address} connected.")
-    while True:
-        try:
-            message = client_socket.recv(1024).decode()
-            if message != "exit":
-                print(f"[{address}] {message}")
-                # Echo the message back to the client
-                client_socket.send(f"Echo: {message}".encode())
-            else:
-                break
-        except socket.error:
-            print(f"[ERROR] Connection with {address} lost.")
-            break
+    global task_start
+    try:
+        # receive num of cores
+        num_of_cores = (client_socket.recv(1024).decode())
+        print(num_of_cores)
+        client_socket.send(task_start)
 
-    client_socket.close()
-    print(f"[DISCONNECT] {address} has disconnected.")
 
+
+
+    except socket.error:
+        print(f"[ERROR] Connection with {address} lost.")
+
+    finally:
+        client_socket.close()
+        print(f"[DISCONNECT] {address} has disconnected.")
 
 
 def main():
+
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.bind((IP, PORT))
     server.listen()
@@ -34,9 +41,11 @@ def main():
 
     while True:
         client_socket, client_address = server.accept()
+        CLIENTS_SOCKETS.append(client_socket)
         thread = threading.Thread(target=handle_client, args=(client_socket, client_address))
         thread.start()
         print(f"[ACTIVE CONNECTIONS] {threading.active_count() - 1}")
+
 
 if __name__ == "__main__":
     main()
